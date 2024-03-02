dafdf4911b4308c703ab49b3a1da9afebd2d9a7b
Iris Bolocan <nexuss13@gmail.com>
2024-03-02 15:45:54 +0200

    classes js

--- dist/scrip.js
+++ dist/scrip.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("11111");
+// console.log("11111")
+class ArmyPerson {
+    constructor(firstName, lastName, phoneNumber, base, salary, unit) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.base = base;
+        this.salary = salary;
+        this.unit = unit;
+    }
+}
+class ArmySoldier extends ArmyPerson {
+    constructor(firstName, lastName, phoneNumber, base, salary, unit, draftDate, releaseDate) {
+        super(firstName, lastName, phoneNumber, base, salary, unit);
+        this.draftDate = draftDate;
+        this.releaseDate = releaseDate;
+    }
+}
