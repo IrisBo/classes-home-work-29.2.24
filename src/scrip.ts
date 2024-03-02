a52c3db59375f9406e75aa4b54476bad0e9c68e7
Iris Bolocan <nexuss13@gmail.com>
2024-03-02 15:44:57 +0200

    added classes

--- src/scrip.ts
+++ src/scrip.ts
@@ -1,1 +1,35 @@
-console.log("11111")
+// console.log("11111")
+
+class ArmyPerson {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    base: string;
+    salary: number;
+    unit: string;
+
+    constructor(firstName: string, lastName: string, phoneNumber: string, base: string, salary: number, unit: string) {
+
+        this.firstName = firstName
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.base = base;
+        this.salary = salary;
+        this.unit = unit;
+    }
+}
+
+class ArmySoldier extends ArmyPerson{
+   readonly draftDate:Date;
+   readonly releaseDate:Date;
+
+    constructor (firstName: string, lastName: string, phoneNumber: string, base: string, salary: number, unit: string, draftDate:Date, releaseDate:Date){
+    super (firstName, lastName, phoneNumber, base, salary, unit);
+    this.draftDate=draftDate;
+    this.releaseDate=releaseDate;
+}
+}
+
+
+
+
